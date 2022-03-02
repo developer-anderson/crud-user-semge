95cc2c237a09f1f8d9f8033777cc897bf98029bc
andersonas <anderson.asnascimentto@gmail.com>
2022-03-02 17:56:56 -0300

    Inclusa de requição para buscar dados de acordo com cep digitado

--- frontend-semge/src/pages/Cadastrar/index.js
+++ frontend-semge/src/pages/Cadastrar/index.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Select from 'react-select';
-import { Container, ConteudoForm, ConteudoTitulo, Titulo, BotaoAcao, ButtonInfo, AlertSuccess, AlertDanger, Form, Label,  Input, ButtonSuccess } from './styles';
+import { Container, ConteudoForm, ConteudoTitulo, Titulo, BotaoAcao, ButtonInfo, AlertSuccess, AlertDanger, Form, Label, Input, ButtonSuccess } from './styles';
+
 
 export const Cadastrar = () => {
-  
+  const [logradouro, setLogradouro] = useState('');
+  const [estado, setEstado] = useState('');
+  const [cidade, setCidade] = useState('');
   const [usuario, setusuario] = useState({
-    titulo: '',
-    descricao: ''
+ 
   });
 
   const [status, setStatus] = useState({
@@ -16,14 +18,28 @@ export const Cadastrar = () => {
   })
 
   const valorInput = e => setusuario(
-    { ...usuario, [e.target.name]: e.target.value }
+    { ...usuario, [e.target.name]: e.target.value },
+    (e.target.value.length >= 8) ? getCep(e.target.value): ''
+    
   );
+  
   const options = [
     { value: 1, label: 'Administrador' },
     { value: 2, label: 'Supervisor' },
     { value: 3, label: 'Operário' },
   ];
-
+  const getCep = async (cep) => {
+    
+    fetch("https://viacep.com.br/ws/"+cep+"/json/")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setEstado(responseJson.uf)
+        setCidade(responseJson.localidade)
+        setLogradouro(responseJson.logradouro)
+        console.log(responseJson)
+      });
+     
+  }
   const cadastrarUsuario = async e => {
     e.preventDefault();
     //console.log(usuario.titulo);
@@ -58,7 +74,7 @@ export const Cadastrar = () => {
   }
 
   return (
-    
+
     <Container>
       <ConteudoForm>
         <ConteudoTitulo>
@@ -74,42 +90,42 @@ export const Cadastrar = () => {
         {status.type === 'success' ? <AlertSuccess>{status.mensagem}</AlertSuccess> : ""}
 
         <Form onSubmit={cadastrarUsuario}>
-         
-        <Label>Nome completo: </Label>
-                    <Input type="text" name="nome" placeholder="nome do usuário" onChange={valorInput} />
 
-                    <Label>CPF: </Label>
-                    <Input type="text" name="cpf"  placeholder="CPF" onChange={valorInput} />
-                    <Label>Telefone: </Label>
-                    <Input type="text" name="telefone"  placeholder="Telefone" onChange={valorInput} />
-                    <Label>E-mail: </Label>
-                    <Input type="email" name="email"  placeholder="E-mail" onChange={valorInput} />
+          <Label>Nome completo: </Label>
+          <Input type="text" name="nome" placeholder="nome do usuário" onChange={valorInput} />
 
-                    <Label>Senha: </Label>
-                    <Input type="password" name="senha" placeholder="Sua senha de acesso" onChange={valorInput} />
-                    
-                    <Label>Data de nascimento: </Label>
-                    <Input type="date" name="data_nascimento" onChange={valorInput} />
+          <Label>CPF: </Label>
+          <Input type="text" name="cpf" placeholder="CPF" onChange={valorInput} />
+          <Label>Telefone: </Label>
+          <Input type="text" name="telefone" placeholder="Telefone" onChange={valorInput} />
+          <Label>E-mail: </Label>
+          <Input type="email" name="email" placeholder="E-mail" onChange={valorInput} />
 
-                    
-                    <Label>Perfil:</Label>
-                    <Select options={options} />
+          <Label>Senha: </Label>
+          <Input type="password" name="senha" placeholder="Sua senha de acesso" onChange={valorInput} />
 
-                    <Label>Cep: </Label>
-                    <Input type="text" name="cep"  onChange={valorInput} />
-                    <Label>Pais: </Label>
-                    <Input type="text" name="pais"  onChange={valorInput} />
-                    <Label>Estado: </Label>
-                    <Input type="text" name="estado"   onChange={valorInput} />
-                    <Label>Cidade: </Label>
-                    <Input type="text" name="cidade"   onChange={valorInput} />
-                    <Label>Lagradouro: </Label>
-                    <Input type="text" name="logradouro"  onChange={valorInput} />
-                    <Label>Complemento: </Label>
-                    <Input type="text" name="complemento"  onChange={valorInput} />
+          <Label>Data de nascimento: </Label>
+          <Input type="date" name="data_nascimento" onChange={valorInput} />
 
-                    <Label>Número: </Label>
-                    <Input type="text" name="numero"  onChange={valorInput} />
+
+          <Label>Perfil:</Label>
+          <Select options={options} />
+
+          <Label>Cep: </Label>
+          <Input type="text" name="cep"  onChange={valorInput} />
+          <Label>Pais: </Label>
+          <Input type="text" name="pais" onChange={valorInput} />
+          <Label>Estado: </Label>
+          <Input type="text" name="estado" value={estado} onChange={valorInput} />
+          <Label>Cidade: </Label>
+          <Input type="text" name="cidade" value={cidade} onChange={valorInput} />
+          <Label>Lagradouro: </Label>
+          <Input type="text" name="logradouro" value={logradouro} onChange={valorInput} />
+          <Label>Complemento: </Label>
+          <Input type="text" name="complemento" onChange={valorInput} />
+
+          <Label>Número: </Label>
+          <Input type="text" name="numero" onChange={valorInput} />
 
           <ButtonSuccess type="submit">Cadastrar</ButtonSuccess>
 
@@ -117,5 +133,5 @@ export const Cadastrar = () => {
       </ConteudoForm>
     </Container>
   );
-  
+
 }
